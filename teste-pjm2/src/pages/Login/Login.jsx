1d76a8d087f4ae6c424e48737948d9dae5cb66df
Alex <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-07-31 11:45:14 -0300

    add teste develop

---
+++ teste-pjm2/src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+function LoginPage() {
+
+    return (
+        <>
+            <div className="login-container">
+                <h1>LOGIN</h1>
+            </div>
+        </>
+    )
+    
+}
+
+export default LoginPage 
